47af00d123e9067643a3c18e1c4a9bbb27307e06
Nam <jjune095@naver.com>
2018-10-02 21:00:27 +0900

    #2 Divide method error

--- src/main/java/edu/handong/csee/pp1/bigdata/lab1_1/Calculator.java
+++ src/main/java/edu/handong/csee/pp1/bigdata/lab1_1/Calculator.java
@@ -74,11 +74,11 @@ public class Calculator {
 	
 	/**
 	 * @param prefix
-	 * @param result
+	 * @param d
 	 * This method prints out computation result.
 	 */
-	void print(String prefix, int result) {
-		System.out.println(prefix + " " + result);
+	void print(String prefix, double d) {
+		System.out.println(prefix + " " + d);
 	}
 
 	int sum(int first, int second) {
@@ -99,10 +99,15 @@ public class Calculator {
 		return first * second;
 	}
 
-	int divide(int first, int second) {
+	double divide(int first, int second) {
 		countForAnyCompution++;
 		localCount++;
-		return first / second;
+		if(second==0) {
+			return Double.NaN;
+		}
+		else 
+			return first / second;
+		
 	}
 
 	int mod(int first, int second) {
